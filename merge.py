f7d88545f524e0b60566e4f50b1fa114f8244263
Andre Frankenthal <as2872@cornell.edu>
2021-04-18 15:39:18 -0500

    Added merge.py cmsRun config to merge AOD files

---
+++ merge.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing('analysis')
+options.register('loginterval', 1000, mytype=VarParsing.varType.int)
+options.parseArguments()
+
+process = cms.Process("PickEvent")
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = options.loginterval
+
+process.source = cms.Source ("PoolSource",
+                fileNames = cms.untracked.vstring(options.inputFiles),
+                        duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+                        )
+
+process.out = cms.OutputModule("PoolOutputModule",
+                fileName = cms.untracked.string(options.outputFile)
+                )
+
+process.end = cms.EndPath(process.out)
